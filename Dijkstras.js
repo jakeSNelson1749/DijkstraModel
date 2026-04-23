88cbe1ba4dc5c7a2f2a9e7e4d0b501dd0db05175
Jake S. Nelson <jakeSNelson17@protonmail.com>
2026-04-23 16:40:02 -0500

    algorithm start

---
+++ Dijkstras.js
@@ -0,0 +1,30 @@
+class Graph{
+    constructor(){
+        //initialize necessary sets and lists
+        this.vertices = new Map();
+        document.getElementById("flag").textContent = "Vertices: EMPTY";
+    }
+    add_vertex(newVertex){
+        if (!this.vertices.has(newVertex)){
+            this.vertices.set(newVertex, new Map());
+        }
+    }
+    print_vertices(){
+        // convert keys into readable strings
+
+        const vertexList = Array.from(this.vertices.keys()).join(", ");
+
+        document.getElementById("flag").textContent = "Vertices: " + vertexList;
+    }
+}
+
+function button_pressed(){
+    index = index +1;
+    //alert(index);
+    myGraph.add_vertex(index.toString());
+    myGraph.print_vertices();
+}
+
+const myGraph = new Graph();
+let index = 0;
+//myGraph.add_vertex("A");
